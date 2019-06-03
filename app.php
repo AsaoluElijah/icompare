7f25ff20eea006e09d2175d903813d88d88169c9
Asaolu Elijah <Asaoluelijah7@gmail.com>
2019-06-03 10:57:33 +0100

    Created an object from twitter oauth api

---
+++ app.php
@@ -0,0 +1,13 @@
+<?php
+	require "twitteroauth/autoload.php";
+	
+	$max = $_REQUEST['a'];
+	$item1 = $_REQUEST['item1'];
+	$item2 = $_REQUEST['item2'];
+	$item3 = $_REQUEST['item3'];
+	
+	use Abraham\TwitterOAuth\TwitterOAuth;
+	$connection = new TwitterOAuth("twqnbQD4rK8iAQzsCpDEuOlTm", "9raK4EaJB243vpuNH8XFxCgjM1tSwkNpvYzFG2w5vhmLCvDcKr", "989188914773622784-YQiScGB3VvWPj5JlIAVXHAC9swPaz4E", "lQZGW7qFQL1fyQ42vkYUoPFLWvSuFi7W5EFQke9ouBYTh");
+	$statuses = $connection->get("search/tweets", ["q" => [$item1, $item2, $item3]], ["max_id" => $max or 100]);
+	
+?>
