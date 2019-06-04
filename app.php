288c3004a748f40662b33b8239e83e00cd44556c
Asaolu Elijah <Asaoluelijah7@gmail.com>
2019-06-04 15:06:54 +0100

    Fixes :octopus:

--- app.php
+++ app.php
@@ -9,5 +9,6 @@
 	use Abraham\TwitterOAuth\TwitterOAuth;
 	$connection = new TwitterOAuth("twqnbQD4rK8iAQzsCpDEuOlTm", "9raK4EaJB243vpuNH8XFxCgjM1tSwkNpvYzFG2w5vhmLCvDcKr", "989188914773622784-YQiScGB3VvWPj5JlIAVXHAC9swPaz4E", "lQZGW7qFQL1fyQ42vkYUoPFLWvSuFi7W5EFQke9ouBYTh");
 	$statuses = $connection->get("search/tweets", ["q" => [$item1, $item2, $item3]], ["max_id" => $max or 100]);
+	echo $statuses;
 	
 ?>
